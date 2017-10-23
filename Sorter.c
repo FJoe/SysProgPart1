d85bc297dfcb9ea5fb8883441cfdfb1725883c71
Francis S. Joe <fsj6@state.cs.rutgers.edu>
2017-10-23 14:14:24 -0400

    Copied all files from Project Part 0

---
+++ Sorter.c
@@ -0,0 +1,228 @@
+#include "Sorter.h"
+#include "MergeSort.c"
+
+char* trimSpace(char* str){
+	int end = strlen(str) - 1;
+	while(str[end] == ' ' || str[end] == '\n' || str[end] == '\r')
+	{
+		str[end] = '\0';
+		end--;
+	}
+	
+	while(*str == ' ')
+	{
+		str++;
+	}
+
+
+
+	return str;
+}
+
+char getDataType(char* data){
+	if(strcmp(data, "color") == 0)
+		return 's';
+ 	else if(strcmp(data, "director_name") == 0)
+		return 's'; 
+	else if(strcmp(data, "actor_2_name") == 0)
+		return 's';
+	else if(strcmp(data, "genres") == 0)
+		return 's';
+	else if(strcmp(data, "actor_1_name") == 0)
+		return 's';
+	else if(strcmp(data, "movie_title") == 0)
+		return 's';
+	else if(strcmp(data, "actor_3_name") == 0)
+		return 's';
+	else if(strcmp(data, "plot_keywords") == 0)
+		return 's';
+	else if(strcmp(data, "movie_imdb_link") == 0)
+		return 's';
+	else if(strcmp(data, "language") == 0)
+		return 's';
+	else if(strcmp(data, "country") == 0)
+		return 's';
+	else if(strcmp(data, "content_rating") == 0)
+		return 's';
+
+	if(strcmp(data, "num_critic_for_reviews") == 0)
+		return 'n';
+	else if(strcmp(data, "duration") == 0)
+		return 'n'; 
+	else if(strcmp(data, "director_facebook_likes") == 0)
+		return 'n'; 
+	else if(strcmp(data, "actor_3_facebook_likes") == 0)
+		return 'n';
+	else if(strcmp(data, "actor_1_facebook_likes") == 0)
+		return 'n';
+	else if(strcmp(data, "gross") == 0)
+		return 'n';
+	else if(strcmp(data, "num_voted_users") == 0)
+		return 'n';
+	else if(strcmp(data, "cast_total_facebook_likes") == 0)
+		return 'n';
+	else if(strcmp(data, "facenumber_in_poster") == 0)
+		return 'n';
+	else if(strcmp(data, "num_user_for_reviews") == 0)
+		return 'n';
+	else if(strcmp(data, "budget") == 0)
+		return 'n';
+	else if(strcmp(data, "title_year") == 0)
+		return 'n';
+	else if(strcmp(data, "actor_2_facebook_likes") == 0)
+		return 'n';
+	else if(strcmp(data, "imdb_score") == 0)
+		return 'n';
+	else if(strcmp(data, "aspect_ratio") == 0)
+		return 'n';
+	else if(strcmp(data, "movie_facebook_likes") == 0)
+		return 'n';
+
+	return 'e';
+		
+}
+
+int main(int argc, char* argv[])
+{
+	//Check if valid input
+	if(argc < 2){
+		printf("ERROR: no arguments\n");
+		return -1;
+	}
+	if(strcmp(argv[1], "-c") != 0){
+		printf("ERROR: not sorting by columns\n");
+		return -1;
+	}
+	if(argc < 3){
+		printf("ERROR: column to sort by not given\n");
+		return -1;
+	}
+
+	//Get column heading and create copy of it
+	char* header = (char*) calloc(1024, sizeof(char));
+	if(fgets(header, sizeof(char) * 1024, stdin) == NULL){
+		printf("ERROR: file empty");
+		return -1;
+	}
+	char* row1 = strdup(header);
+
+	//If column to search for is found, get column number
+	char* colToSort = argv[2];
+	int colNumToSort = 0;
+	const char delim[2] = ",";
+	const char otherDelim[3] = "\"";
+	char* curHead = strsep(&row1, delim);
+	while( curHead != NULL && strcmp(colToSort, trimSpace(curHead)) != 0){
+		curHead = strsep(&row1, delim);
+		colNumToSort++;
+	}
+	if(curHead == NULL){
+		printf("ERROR: column to sort not found");
+		return -1;
+	}
+
+	//Print column headers to output file
+	printf("%s", header);
+
+	//Creating list of DataRow
+	DataRow** list = (DataRow**) malloc(sizeof(DataRow*) * 20000);
+	char dataType = getDataType(colToSort);
+	int curRowNum = 0;
+	
+	char* origRow = (char*) calloc(1024, sizeof(char));
+	
+	//Goes through each row in csv file
+	while(fgets(origRow, sizeof(char) * 1024, stdin) != NULL){
+		//New dataRow for array
+		DataRow* newDataRow = (DataRow*) malloc(sizeof(DataRow));
+		newDataRow->dataType = dataType;
+
+		char* rowDelim = strdup(origRow);
+
+		//newRow is data for new row with trimmed spaces
+		char* newRow = (char*) calloc(1024, sizeof(char));
+
+		//Goes through each word, trimming spaces and finding data to sort by and add data to array
+		char* curWord = strsep(&rowDelim, delim);
+		int firstWord = 1;
+		int i = 0;
+		while(curWord != NULL){
+			if(firstWord)
+				firstWord = 0;
+			else
+				strcat(newRow, ",");
+
+			char wordToAdd[300];  
+			strcpy(wordToAdd, curWord);
+			
+
+			if(wordToAdd[0] == '"' && wordToAdd[strlen(wordToAdd) - 1] != '"')
+			{
+				char* otherHalf = strsep(&rowDelim, otherDelim);
+
+				strcat(wordToAdd, ",");
+				strcat(wordToAdd, otherHalf);	
+				strcat(wordToAdd, "\"");		
+
+				curWord = strsep(&rowDelim, delim);
+			}
+			char* trimWord = trimSpace(wordToAdd);
+
+			if(i == colNumToSort){
+				DataCompare* newDataCompare = (DataCompare*) malloc(sizeof(DataCompare));
+				if(dataType == 'n'){
+					newDataCompare->numData = atof(trimWord);
+				}
+				else if(dataType == 's'){
+					newDataCompare->stringData = trimWord;
+				}
+
+				newDataRow->dataCompare = newDataCompare;
+			}
+
+			strcat(newRow, trimWord);
+			curWord = strsep(&rowDelim, delim);
+
+			i++;
+		}
+		newDataRow->data = newRow;
+		list[curRowNum] = newDataRow;
+		curRowNum++;
+	}
+
+	//Sort list
+	mergeSort(list, 0, (curRowNum -1));
+
+	//Prints list then frees it
+	int j;	
+	for(j = 0; j < curRowNum; j++)
+	{
+		printf("%s\n", list[j]->data);
+
+		free(list[j]->dataCompare);
+		free(list[j]->data);
+		free(list[j]);
+	}
+	free(header);
+	free(origRow);
+	free(list);
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Sorter.h
@@ -0,0 +1,50 @@
+/*****
+*
+*	Define structures and function prototypes for your sorter
+*
+*
+*
+******/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifndef SORTER_H
+#define SORTER_H
+
+//data to compare each row by. Either comparing data of string or numeric values
+union data_compare{
+	double numData;
+	char* stringData;
+};
+typedef union data_compare DataCompare;
+
+
+//single row of csv file. Has content of the csv file, data to compare each row with, and the datatype of the data to compare with
+struct data_row{
+
+/**
+	dataType:
+		n = numData
+		s = stringData
+**/
+	char dataType;
+	char* data;
+	DataCompare* dataCompare;	
+};
+typedef struct data_row DataRow;
+
+//Trims leading and trailing spaces from a string
+char* trimSpace(char* str);
+
+//Gets dataType of column name
+char getDataType(char* data);
+
+//Suggestion: prototype a mergesort function
+void merge(DataRow ** array, int left, int mid, int right);
+void mergeSort(DataRow ** array, int left, int right);
+
+
+
+#endif
